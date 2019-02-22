93e01be9f70742ec805a4c652d10b62183ea4276
tukhtarov <tukhtarov@gmail.com>
2019-02-22 16:24:58 +0700

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,109 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Сложение дробей</h1>
+    <div class="container">
+      <FractionComponent
+        v-for="(fraction, index) in fractions"
+        :key="fraction"
+        :index="index"
+        :fraction="fraction"
+        :onDeleteFraction="deleteFraction"
+        :onInputkeyDown="inputkeyDown"
+        :onChangeFractionValue="changeFractionValue"
+        :fractionsNumber="fractions.length"
+      />
+      <div class="equality-sign">=</div>
+      <div class="result-text">{{ fractionsAddition }}</div>
+    </div>
+    <button @click="addFraction">Добавить дробь</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FractionComponent from "./components/FractionComponent.vue";
 
 export default {
-  name: 'app',
+  name: "app",
+  methods: {
+    inputkeyDown: function(inputValue, e) {
+      const symbol = event.key;
+      if (isNaN(symbol) && symbol !== "Backspace") {
+        event.preventDefault();
+      } else if (inputValue.length === 0 && symbol == 0) {
+        event.preventDefault();
+      }
+    },
+    changeFractionValue: function(index, property, e) {
+      this.$data.fractions[index][property] = e.target.value;
+    },
+    addFraction: function() {
+      if (this.$data.fractions.length < 5) {
+        this.$data.fractions.push({
+          numerator: "",
+          denominator: ""
+        });
+      }
+    },
+    deleteFraction: function(index) {
+      this.$data.fractions.splice(index, 1);
+    }
+  },
+  data: function() {
+    return {
+      fractions: [
+        {
+          numerator: "",
+          denominator: ""
+        },
+        {
+          numerator: "",
+          denominator: ""
+        }
+      ]
+    };
+  },
+  computed: {
+    fractionsAddition: function() {
+      if (
+        this.$data.fractions.some(item => !item.numerator || !item.denominator)
+      ) {
+        return "Заполните все поля";
+      }
+
+      return this.$data.fractions.reduce(
+        (result, item) => result + item.numerator / item.denominator,
+        0
+      );
+    }
+  },
   components: {
-    HelloWorld
+    FractionComponent
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.container {
+  margin: 70px 0;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+.result-text {
+  margin: 0 20px;
+  font-size: 30px;
+  font-weight: bold;
+}
+.equality-sign {
+  font-size: 32px;
 }
 </style>
---
+++ src/components/FractionComponent.vue
@@ -0,0 +1,85 @@
+<template>
+  <transition name="fade">
+    <div class="fraction-container">
+      <div class="fraction">
+        <input
+          @input="onChangeFractionValue(index, 'numerator', ...arguments)"
+          :value="fraction.numerator"
+          @keydown="onInputkeyDown(fraction.numerator, ...arguments)"
+          maxlength="2"
+        >
+        <div class="divider"/>
+        <input
+          @input="onChangeFractionValue(index, 'denominator', ...arguments)"
+          :value="fraction.denominator"
+          @keydown="onInputkeyDown(fraction.denominator, ...arguments)"
+          maxlength="2"
+        >
+      </div>
+      <div class="plus-sign" v-if="index < fractionsNumber-1">+</div>
+      <img
+        :src="require('../assets/delete-icon.png')"
+        class="delete-button"
+        v-if="fractionsNumber > 2"
+        @click="onDeleteFraction(index)"
+      >
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: "FractionComponent",
+  props: {
+    fraction: Object,
+    onDeleteFraction: Function,
+    onChangeFractionValue: Function,
+    onInputkeyDown: Function,
+    fractionsNumber: Number,
+    index: Number
+  }
+};
+</script>
+
+<style scoped>
+.fraction-container {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  position: relative;
+}
+.fraction {
+  display: flex;
+  flex-direction: column;
+  width: 70px;
+  margin: 0 15px;
+}
+.divider {
+  width: 100%;
+  border-bottom: 2px solid black;
+  margin: 10px 0;
+}
+.delete-button {
+  position: absolute;
+  top: -30px;
+  left: 90px;
+  width: 20px;
+  height: 20px;
+  cursor: pointer;
+}
+input {
+  height: 50px;
+  font-size: 42px;
+}
+.plus-sign {
+  font-size: 32px;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
